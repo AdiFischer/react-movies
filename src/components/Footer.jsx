4323a57add2f3bbdf9176bdcd78baf5a3ed3253e
AdiFischer <adifischer1@gmail.com>
2022-10-27 16:16:38 -0400

    horror movie site

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+function Footer() {
+    return(
+        <footer>
+            <p>&copy; 2022 Adi Fischer.
+                Source: <a href="https://sampleapis.com/api-list/movies">
+                    sampleapis.com
+                </a>
+
+            </p>
+        </footer>
+        
+    )
+}
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+function Header() {
+    return (
+        <header>
+            <h1>React movies</h1>
+        </header>
+    )
+}
+export default Header;
